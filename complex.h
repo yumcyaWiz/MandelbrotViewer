c9ee15e324bc59a06e0bf09fd0fb9aedab635cde
yumcyawiz <hashdolf1923@gmail.com>
2019-10-03 05:38:50 +0900

    render mandelbrot

--- complex.h
+++ complex.h
@@ -1,6 +1,8 @@
 #ifndef COMPLEX_H
 #define COMPLEX_H
 
+#include <cmath>
+
 
 template <typename T>
 struct Complex {
@@ -29,4 +31,14 @@ Complex<T> operator/(const Complex<T>& c1, const Complex<T>& c2) {
   const T div = c2.real*c2.real + c2.imag*c2.imag;
   return Complex<T>((c2.real*c1.real + c2.imag*c1.imag)/div, (c2.real*c1.imag - c2.imag*c1.real)/div);
 }
+
+
+template <typename T>
+double length(const Complex<T>& c) {
+  return std::sqrt(c.real*c.real + c.imag*c.imag);
+}
+
+
+using Complex_d = Complex<double>;
+using Complex_f = Complex<float>;
 #endif
--- main.cpp
+++ main.cpp
@@ -1,45 +1,85 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
 
-#include "glad/glad.h"
 #include <GLFW/glfw3.h>
 
 #include "imgui.h"
 #include "examples/imgui_impl_glfw.h"
-#include "examples/imgui_impl_opengl3.h"
+#include "examples/imgui_impl_opengl2.h"
+
+#include "complex.h"
+
+
+int gWidth = 512;
+int gHeight = 512;
+int gMaxIterate = 100;
+std::vector<uint8_t> gPixelBuffer(3 * gWidth * gHeight);
+
+
+void renderMandelbrot(std::vector<uint8_t>& pixelBuffer) {
+  for(int j = 0; j < gHeight; j++) {
+    for(int i = 0; i < gWidth; i++) {
+      Complex_d c(double(i)/gWidth, double(j)/gHeight);
+      Complex_d z(0, 0);
+
+      int break_iter = 0;
+      for(int k = 0; k < gMaxIterate; k++) {
+        z = z*z + c;
+        if (length(z) > 2.0) {
+          break_iter = k;
+          break;
+        }
+      }
+
+      pixelBuffer[0 + 3*i + gWidth*j] = std::min(int(double(break_iter)/gMaxIterate * 255), 255);
+      pixelBuffer[1 + 3*i + gWidth*j] = std::min(int(double(break_iter)/gMaxIterate * 255), 255);
+      pixelBuffer[2 + 3*i + gWidth*j] = std::min(int(double(break_iter)/gMaxIterate * 255), 255);
+    }
+  }
+}
 
 
 int main() {
+  //Initialize GLFW
   if (!glfwInit()) {
     return -1;
   }
-  const char* glsl_version = "#version 150";
+  const char* glsl_version = "#version 130";
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-  GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(512, 512, "Dear ImGui", NULL, NULL);
   if (window == NULL) {
     return -1;
   }
   glfwMakeContextCurrent(window);
   glfwSwapInterval(1);
 
+  //Initialize GLAD
+  /*
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     std::cerr << "failed to initialize glad" << std::endl;
     return -1;
   }
+  */
 
+  //Initialize ImGui
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
   ImGuiIO& io = ImGui::GetIO(); (void)io;
 
   ImGui_ImplGlfw_InitForOpenGL(window, true);
-  ImGui_ImplOpenGL3_Init(glsl_version);
+  ImGui_ImplOpenGL2_Init();
+  //ImGui_ImplOpenGL3_Init(glsl_version);
 
+  renderMandelbrot(gPixelBuffer);
+
+  //Rendering Loop
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
 
-    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplOpenGL2_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
@@ -52,11 +92,13 @@ int main() {
     glfwGetFramebufferSize(window, &display_w, &display_h);
     glViewport(0, 0, display_w, display_h);
     glClear(GL_COLOR_BUFFER_BIT);
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+    glDrawPixels(gWidth, gHeight, GL_RGB, GL_UNSIGNED_BYTE, gPixelBuffer.data());
+
     glfwSwapBuffers(window);
   }
 
-  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplOpenGL2_Shutdown();
   ImGui_ImplGlfw_Shutdown();
   ImGui::DestroyContext();
 
