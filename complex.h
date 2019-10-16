af2be2cecfa28d15370aba4fc4bfb1d018c01b2c
yumcyawiz <hashdolf1923@gmail.com>
2019-10-16 14:22:16 +0900

    make inline

--- complex.h
+++ complex.h
@@ -16,32 +16,32 @@ struct Complex {
 
 
 template <typename T>
-Complex<T> operator+(const Complex<T>& c1, const Complex<T>& c2) {
+inline Complex<T> operator+(const Complex<T>& c1, const Complex<T>& c2) {
   return Complex<T>(c1.real + c2.real, c1.imag + c2.imag);
 }
 template <typename T>
-Complex<T> operator-(const Complex<T>& c1, const Complex<T>& c2) {
+inline Complex<T> operator-(const Complex<T>& c1, const Complex<T>& c2) {
   return Complex<T>(c1.real - c2.real, c1.imag - c2.imag);
 }
 template <typename T>
-Complex<T> operator*(const Complex<T>& c1, const Complex<T>& c2) {
+inline Complex<T> operator*(const Complex<T>& c1, const Complex<T>& c2) {
   return Complex<T>(c1.real*c2.real - c1.imag*c2.imag, c1.real*c2.imag + c2.real*c1.imag);
 }
 template <typename T>
-Complex<T> operator/(const Complex<T>& c1, const Complex<T>& c2) {
+inline Complex<T> operator/(const Complex<T>& c1, const Complex<T>& c2) {
   const T div = c2.real*c2.real + c2.imag*c2.imag;
   return Complex<T>((c2.real*c1.real + c2.imag*c1.imag)/div, (c2.real*c1.imag - c2.imag*c1.real)/div);
 }
 
 
 template <typename T>
-double length(const Complex<T>& c) {
+inline double length(const Complex<T>& c) {
   return std::sqrt(c.real*c.real + c.imag*c.imag);
 }
 
 
 template <typename T>
-std::ostream& operator<<(std::ostream& stream, const Complex<T>& c) {
+inline std::ostream& operator<<(std::ostream& stream, const Complex<T>& c) {
   stream << "(" << c.real << ", " << c.imag << ")";
   return stream;
 }
